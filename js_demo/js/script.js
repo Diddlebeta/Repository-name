9807c0f098ce44b4d1a898d733ccdcf5c98de923
WAZAAAAAAAAAAAAAAAAA <tvarma@eastsideprep.org>
2025-11-06 14:39:02 -0800

    Add files via upload

---
+++ js_demo/js/script.js
@@ -0,0 +1,6 @@
+function askQuestion() {
+    var p = prompt('WHAT IS THE MEANING OF THE UNIVERSE');
+    if (p !=null) {
+        document.getElementById('question').innerHTML = 'uhhh ' + p + ' I am NOT that smart😭😭🙏';
+    }
+}
